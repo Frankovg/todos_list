9cb5c5d2a3f9bf0b6eb4cad9832b5e0c42ad60b6
Franco Amoroso <di.francoamoroso@gmail.com>
2022-08-20 16:18:52 +0200

    Updates

--- src/EmptyTodos/index.js
+++
@@ -1,7 +0,0 @@
-import React from "react";
-
-function EmptyTodos() {
-  return <p>Create your first TODO!</p>;
-}
-
-export { EmptyTodos };
--- src/TodosError/index.js
+++
@@ -1,6 +0,0 @@
-import React from "react";
-function TodosError({ error }) {
-  return <p>Oops! An Error Occurred...</p>;
-}
-
-export { TodosError };
